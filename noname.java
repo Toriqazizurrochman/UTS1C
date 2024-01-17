2975ac1c4ae025ecc6edfe04abfe17b79274bff1
root <root@LAPTOP-M5QUC8T1.localdomain>
2024-01-17 11:51:48 +0700

    Initial commit

---
+++ noname.java
@@ -0,0 +1,9 @@
+public class ContohMethod {
+    public static void main(string[] args) throws IOException {
+        //penjumlahan dua angka
+        CetakJudul(); function / method
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)):
+        System.out.print(Masukan Angka 1: ");
+                int x = Integrar.parseInt(reader.readLine())
+    }
+}
